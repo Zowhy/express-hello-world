4b7b061fb5a02042189d93283a19f2f35718cc82
Daniel <daniel.silva20@estudante.ifms.edu.br>
2024-12-09 08:28:48 -0400

    Desafio #1 - Retornando Meu nome
    Adicionado endpoint /meunome que retorna meu nome completo

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+  res.send('Meu nome é Daniel [Seu Sobrenome]');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
